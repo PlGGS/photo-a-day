3275c5715c4ddb40618db5733fe7b4cb3b14478d
PlGGS <bbor0422@gmail.com>
2022-12-02 03:48:20 -0600

    Added a basic photo viewer to the project section

---
+++ lib/widgets/views/project/Project.dart
@@ -0,0 +1,20 @@
+//basically the twitter timeline view
+
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+
+class Project extends StatelessWidget {
+  const Project({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PhotoView(
+      imageProvider: const NetworkImage("https://blakeboris.com/cubs.jpg"),
+      minScale: PhotoViewComputedScale.contained,
+      maxScale: PhotoViewComputedScale.contained,
+      backgroundDecoration: BoxDecoration(
+        color: Theme.of(context).canvasColor,
+      ),
+    );
+  }
+}
